8dfa0a21c05e8b033d17c3303d87b89a3577ecb8
RafalWyszynski <rafalwys@onet.pl>
2023-05-24 03:22:32 +0200

    Split and Scale Data

--- project.py
+++ project.py
@@ -1,9 +1,20 @@
+from keras.models import Sequential
+from keras.layers import Dense
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import BatchNormalization
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
-#Load Footballers stats from .csv file
+# Setting models' seed
+np.random.seed(120)
+
+# Loading Footballers stats from .csv file
 stats = pd.read_csv('PlayersStats.csv', encoding='Windows-1250', index_col=1, sep=';')
 
-# Sprawdzenie ogólnych informacji o tabeli
+# Checking DataFrame info
 print(stats.info())
 print(stats.head())
 
@@ -14,3 +25,15 @@ stats = stats.drop(["Squad", "Comp", "Born", "Rk", "Nation", "Age", "MP", "Start
 stats = stats.dropna()
 stats["Pos"] = stats["Pos"].str.slice(stop=2)
 print(stats.head())
+
+# Data Spliting
+X = stats.drop(['Pos'], axis=1).values
+y = stats['Pos'].values
+y = pd.Categorical(y).codes
+y = to_categorical(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
+
+#Data Scaling
+scaler = StandardScaler()
+X_train= scaler.fit_transform(X_train)
+X_test= scaler.fit_transform(X_test)
